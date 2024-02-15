428387be98a2f4f27ae0ec2ac301d8722e2c8a53
Мурат Кертиев <kertiev1107@gmail.com>
2024-02-15 18:00:40 +0300

    input, output views

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm
+from django.contrib.auth.decorators import login_required
 
 
 def user_login(request):
@@ -15,7 +16,7 @@ def user_login(request):
             if user is not None:
                 if user.is_active:
                     login(request, user)
-                    return HttpResponse('Authenticated successfuly')
+                    return HttpResponse('Authenticated successfully')
                 else:
                     return HttpResponse('Disabled account')
             else:
@@ -23,3 +24,10 @@ def user_login(request):
     else:
         form = LoginForm()
     return render(request, 'account/login.html', {'form': form})
+
+
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
