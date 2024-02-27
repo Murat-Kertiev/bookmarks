739cca58d02e8c796e7a4547bcb4ba563490b946
Мурат Кертиев <kertiev1107@gmail.com>
2024-02-27 23:32:29 +0300

    distribution of media files

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -35,18 +35,14 @@ def dashboard(request):
 
 
 def register(request):
+    user_form = UserRegistrationForm(request.POST or None)
     if request.method == 'POST':
-        user_form = UserRegistrationForm(request.POST)
-        # Создать новый объект пользователя,
-        # но пока не сохранять его
-        new_user = user_form.save(commit=False)
-        # Установить выбранный пароль
-        new_user.set_password(
-            user_form.cleaned_data['password']
-        )
-        # Сохранить объект User
-        new_user.save()
-        return render(request, 'account/register_done.html', {'new_user': new_user})
-    else:
-        user_form = UserRegistrationForm()
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            print(user_form.cleaned_data)
+            new_user.save()
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
     return render(request, 'account/register.html', {'user_form': user_form})
