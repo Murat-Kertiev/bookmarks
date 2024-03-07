5fe77f45fb399f58b9caa48e66dc6a6d9a527290
Мурат Кертиев <kertiev1107@gmail.com>
2024-03-07 13:46:58 +0300

    creating a many-to-many relationship between user and image models, registration imaage model on the admin site

--- bookmarks/images/models.py
+++ bookmarks/images/models.py
@@ -9,12 +9,15 @@ class Image(models.Model):
         related_name='images_created',
         on_delete=models.CASCADE
     )
-    title = models.CharField(max_lenght=200)
+    title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
     url = models.URLField(max_length=2000)
     image = models.ImageField(upload_to='images/%Y/%m/%d')
     description = models.TextField(blank=True)
     created = models.DateField(auto_now_add=True)
+    users_like = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                        related_name='images_liked',
+                                        blank=True)
 
     class Meta:
         indexes = [
