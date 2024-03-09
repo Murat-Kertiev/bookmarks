7056a03d58877307841595fbd26f21e42ca47a98
Мурат Кертиев <kertiev1107@gmail.com>
2024-03-09 16:05:18 +0300

    Overriding the save() method of the ImageCreateForm class

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Image
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
+import requests
 
 
 class ImageCreateForm(forms.ModelForm):
@@ -13,9 +16,26 @@ class ImageCreateForm(forms.ModelForm):
 
     def clean_url(self):
         url = self.cleaned_data['url']
-        valid_extensions = ['jpg', 'jpeg', 'png']
-        # получаем расширение файла из url-адреса
+        valid_extension = ['jpg', 'jpeg', 'png']
+        # извлекаем расширение файла из url-адреса изображения
         extension = url.rsplit('.', 1)[1].lower()
-        if extension not in valid_extensions:
+        if extension not in valid_extension:
             raise forms.ValidationError('The given URL does not match valid image extensions.')
         return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        # возвращает объект модели Image без его сохранения в бд
+        image = super().save(commit=False)
+        image_url = self.cleaned_data['url']
+        name = slugify(image.title)
+        extension = image_url.rsplit('.', 1)[1].lower()
+        image_name = f'{name}.{extension}'
+        # скачать изображение с данного URL-адреса
+        response = requests.get(image_url)
+        image.image.save(image_name,
+                         ContentFile(response.content),
+                         save=False)
+        if commit:
+            image.save()
+        return image
+    
