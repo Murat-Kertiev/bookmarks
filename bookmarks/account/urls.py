fa9c70763c37c3322d031413674cd5e46bca8451
Мурат Кертиев <kertiev1107@gmail.com>
2024-03-18 22:40:48 +0300

    List views and detailed information for user profiles have been created

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -30,9 +30,11 @@ urlpatterns = [
     # path('password-reset/complete/',
     #      auth_views.PasswordResetCompleteView.as_view(),
     #      name='passowrd_reset_complete'),
-    
+
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name="dashboard"),
     path('register/', views.register, name='register'),
     path('edit/', views.edit, name='edit'),
+    path('users/', views.user_list, name='user_list'),
+    path('users/<username>', views.user_detail, name='user_detail'),
 ]
